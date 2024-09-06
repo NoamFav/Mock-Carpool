b943261686425b347ed6ed931432eeaf8a9cb433
noamfav <noam.favier@icloud.com>
2024-09-06 11:12:24 +0200

    Initial Commit

---
+++ Mock-Carpool/Mock_CarpoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mock_CarpoolApp.swift
+//  Mock-Carpool
+//
+//  Created by Boss on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Mock_CarpoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mock-CarpoolTests/Mock_CarpoolTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mock_CarpoolTests.swift
+//  Mock-CarpoolTests
+//
+//  Created by Boss on 06/09/2024.
+//
+
+import Testing
+@testable import Mock_Carpool
+
+struct Mock_CarpoolTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

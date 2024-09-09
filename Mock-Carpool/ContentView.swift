ef4c97d07980763d0466c126d1410ec803e4c9f9
noamfav <noam.favier@icloud.com>
2024-09-09 22:10:33 +0200

    abandonned the autocomplete thing, apple map sucks for that, improved the UI by a lot tho, it's starting to look great

--- Mock-Carpool/ContentView.swift
+++ Mock-Carpool/ContentView.swift
@@ -10,10 +10,9 @@ import MapKit
 
 struct ContentView: View {
     
+    // MARK: - State Variables
     @State private var startLocation: String = ""
     @State private var endLocation: String = ""
-    @State private var startCompletions: [MKLocalSearchCompletion] = []
-    @State private var endCompletions: [MKLocalSearchCompletion] = []
     
     @State private var isStartFieldActive: Bool = false
     @State private var isEndFieldActive: Bool = false
@@ -24,92 +23,131 @@ struct ContentView: View {
     
     @State private var mapRegion: MKCoordinateRegion?
     
-    private var startAutocompleteDelegate = AutocompleteDelegate(completions: .constant([]))
-    private var endAutocompleteDelegate = AutocompleteDelegate(completions: .constant([]))
+    @State private var isRouteSearching: Bool = false
     
-    let completer = MKLocalSearchCompleter()
+    @State private var routeDistance: String = ""
+    @State private var routeTravelTime: String = ""
     
+    // MARK: - View Body
     var body: some View {
-        
         ZStack(alignment: .bottom) {
             
+            // Background Gradient
+            LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.4), Color.purple.opacity(0.7)]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            // Map view with safe area ignored
             MapView(routePolyline: $routePolyline, mapItems: $mapItems, region: $mapRegion)
                 .ignoresSafeArea()
             
-            VStack(spacing: 0) {
-                TextField("Start location", text: $startLocation, onEditingChanged: { isEditing in
-                        isStartFieldActive = isEditing
-                        isEndFieldActive = !isEditing
-                    })
-                .onChange(of: startLocation) {
-                    updateAucompleteResults(for: $startCompletions, with: startLocation)
-                    updateMapRegionBasedOnLocations()
-                }
-                .padding(.top)
-                .padding(.horizontal)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                
-                if isStartFieldActive && !startCompletions.isEmpty {
-                    List(startCompletions, id: \.self) { completion in
-                        Text(completion.title)
-                            .onTapGesture {
-                                self.startLocation = completion.title
-                                self.isStartFieldActive = false
-                                startCompletions.removeAll()
-                            }
-                    }
-                    .frame(maxHeight: 200)
-                    .padding(.horizontal)
-                    .clipShape(RoundedRectangle(cornerRadius: 8))
-                }
-                
-                TextField("End location", text: $endLocation, onEditingChanged: { isEditing in
-                    isEndFieldActive = isEditing
-                    isStartFieldActive = false
-                })
-                .onChange(of: endLocation) {
-                    updateAucompleteResults(for: $endCompletions, with: endLocation)
-                    updateMapRegionBasedOnLocations()
-                }
-                .padding(.horizontal)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                
-                if isEndFieldActive && !endCompletions.isEmpty {
-                    List(endCompletions, id: \.self) { completion in
-                        Text(completion.title)
-                            .onTapGesture {
-                                self.endLocation = completion.title
-                                self.isEndFieldActive = false
-                                endCompletions.removeAll()
-                            }
-                    }
-                    .frame(maxHeight: 200)
-                    .padding(.horizontal)
-                    .clipShape(RoundedRectangle(cornerRadius: 8))
-                }
-                Button(action: searchForRoute) {
-                    Text("Get route")
+            // UI Elements stacked vertically
+            VStack(spacing: 16) {
+                // Start Location TextField
+                HStack {
+                    Image(systemName: "mappin.and.ellipse")
+                        .foregroundColor(.red)
                         .padding()
-                        .background(Color.blue)
+                        .background(Color(white: 0.15))
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle()
+                                .stroke(Color.white.opacity(0.6), lineWidth: 1) // Light stroke for definition
+                        )
+                    
+                    TextField("Start location", text: $startLocation)
+                        .padding()
+                        .background(Color(white: 0.15))
                         .foregroundColor(.white)
-                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.white.opacity(0.6), lineWidth: 1) // Light stroke for definition
+                        )
                 }
-                .padding(.vertical)
+                .padding(.horizontal)
+                .padding(.top)
+                
+                // End Location TextField with better contrast
+                HStack {
+                    Image(systemName: "flag.fill")
+                        .foregroundColor(.green)
+                        .padding()
+                        .background(Color(white: 0.15))
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle()
+                                .stroke(Color.white.opacity(0.6), lineWidth: 1) // Light stroke for definition
+                        )
+                    
+                    TextField("End location", text: $endLocation)
+                        .padding()
+                        .background(Color(white: 0.15))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.white.opacity(0.6), lineWidth: 1)
+                        )
+                }
+                .padding(.horizontal)
+                
+                // Get Route Button with animation
+                HStack {
+                    Button(action: {
+                        isRouteSearching = true
+                        searchForRoute()
+                    }) {
+                        ZStack {
+                            // Background Layer
+                            if isRouteSearching {
+                                Color.gray
+                            } else {
+                                LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                            }
+                            
+                            // Button Content (Icon/Text)
+                            Text(Image(systemName: "location.fill"))
+                                .fontWeight(.bold)
+                                .padding()
+                                .foregroundColor(.white)
+                                .clipShape(Capsule())
+                                .scaleEffect(isRouteSearching ? 1.1 : 1.0)
+                                .animation(.easeInOut, value: isRouteSearching)
+                        }
+                        .frame(width: 70, height: 70)
+                        .clipShape(Circle())
+                        .shadow(color: .gray, radius: 5, x: 0, y: 5)
+                    }
+                    
+                    if !routeDistance.isEmpty && !routeTravelTime.isEmpty {
+                        VStack {
+                            Text("Distance: \(routeDistance)")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                            
+                            Text("Travel Time: \(routeTravelTime)")
+                                .font(.subheadline)
+                                .foregroundColor(.white)
+                        }
+                        .padding()
+                        .background(Color.gray.opacity(0.8))
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                    }
+                }
+                .padding(.bottom)
             }
-            .background(Color.gray.opacity(0.5))  // Add a background with some opacity for better visibility
-            .cornerRadius(12) // Optional, for rounded corners on the container
+            .background(Color.gray.opacity(0.9))
+            .cornerRadius(20)
+            .shadow(radius: 10)
             .padding(.horizontal)
             .ignoresSafeArea()
         }
     }
     
-    func updateAucompleteResults(for completions: Binding<[MKLocalSearchCompletion]>, with query: String) {
-        print("updating autocomplete")
-        completer.queryFragment = query
-        completer.resultTypes = .address
-        completer.delegate = isStartFieldActive ? startAutocompleteDelegate : endAutocompleteDelegate
-    }
+    // MARK: - Helper Functions
     
+    /// Initiates a search for the route between start and end locations.
     func searchForRoute() {
         let startRequest = MKLocalSearch.Request()
         startRequest.naturalLanguageQuery = startLocation
@@ -120,23 +158,32 @@ struct ContentView: View {
         let startSearch = MKLocalSearch(request: startRequest)
         let endSearch = MKLocalSearch(request: endRequest)
         
+        // Search for start location
         startSearch.start { startResponse, error in
             if let startItem = startResponse?.mapItems.first {
+                // Search for end location
                 endSearch.start { endResponse, error in
                     if let endItem = endResponse?.mapItems.first {
                         self.mapItems = [startItem, endItem]
                         calculateRoute(from: startItem, to: endItem)
+                        isRouteSearching = false
                     }
                 }
+            } else {
+                isRouteSearching = false
             }
         }
     }
     
+    /// Calculates the route between two MKMapItems.
+    /// - Parameters:
+    ///   - start: Starting MKMapItem.
+    ///   - end: Destination MKMapItem.
     func calculateRoute(from start: MKMapItem, to end: MKMapItem) {
         let directionRequest = MKDirections.Request()
         directionRequest.source = start
         directionRequest.destination = end
-        directionRequest.transportType = .automobile // You can change this to walking, transit, etc.
+        directionRequest.transportType = .automobile
         
         let directions = MKDirections(request: directionRequest)
         directions.calculate { response, error in
@@ -149,39 +196,31 @@ struct ContentView: View {
                 DispatchQueue.main.async {
                     self.directions = response
                     self.routePolyline = route.polyline
+                    
+                    let distanceInKilometers = route.distance / 1000
+                    let durationInMinutes = route.expectedTravelTime / 60
+                    
+                    self.routeDistance = String(format: "%.2f km", distanceInKilometers)
+                    self.routeTravelTime = String(format: "%.0f min", durationInMinutes)
                 }
             }
         }
     }
     
+    /// Updates the map region based on start and end locations.
     func updateMapRegionBasedOnLocations() {
         if let startItem = mapItems.first {
-            // If only start location is available
-            let region = MKCoordinateRegion(center: startItem.placemark.coordinate, latitudinalMeters: 5000, longitudinalMeters: 5000)
+            let region = MKCoordinateRegion(
+                center: startItem.placemark.coordinate,
+                latitudinalMeters: 5000,
+                longitudinalMeters: 5000
+            )
             self.mapRegion = region
         }
     }
 }
 
-class AutocompleteDelegate: NSObject, MKLocalSearchCompleterDelegate {
-    @Binding var completions: [MKLocalSearchCompletion]
-
-    init(completions: Binding<[MKLocalSearchCompletion]>) {
-        _completions = completions
-    }
-
-    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
-        DispatchQueue.main.async {
-            self.completions = completer.results
-            print("Completions updated: \(self.completions.map { $0.title })")
-        }
-    }
-
-    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
-        print("Autocomplete failed with error: \(error)")
-    }
-}
-
+// MARK: - Preview
 #Preview {
     ContentView()
 }
